4cbb72a4ee126716c5652e1cb3f64e82a31e04f8
Haley Akinlabi (haleyakinlabi) <haley.akinlabi@clickfunnels.com>
2023-07-27 10:25:52 -0600

    password update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,5 @@
 class MainController < ApplicationController
     def index
-    end
-end
-class MainController < ApplicationController
-    def index
-        if session[:user_id]
-            @user = User.find(session[:user_id])
-        end
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
   #GET /about 
   get "about", to: "about#index", as: :about
+  
   #Root 
   root to: "main#index"
+  
   #Registration
   get "sign_up", to: "registrations#new"
   #Registration Submit Form 
   post "sign_up", to: "registrations#create"
+
+  #Sign In
+  get "sign_in", to: "sessions#new"
+  #Sign In Submit Form
+  post "sign_in", to: "sessions#create"
+  
+  #Logout
+  delete "logout", to: "sessions#destroy"
+
+  #Password Editing
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
 end
