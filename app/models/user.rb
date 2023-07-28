41c80e70f46014b94551c7c99bd2dd4bd59ed463
Haley Akinlabi (haleyakinlabi) <haley.akinlabi@clickfunnels.com>
2023-07-28 14:04:07 -0600

    twitter accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
+
     has_secure_password
     validates :email, presence: true, format: { with: /@/ , message: "must be a valid email address" }
 end
